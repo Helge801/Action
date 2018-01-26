309c81627dfc33d9251f2fe7cc59c95161166988
Bryan Lund <bryan.helge@gmail.com>
2018-01-26 11:15:26 -0700

    added users to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,19 @@ def em output
   puts "*" * 200
 end
 
+em "Creating Users"
+
+User.create!(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+
+em "User created with test@test.com & asdfasdf"
+
+5.times do |i|
+ user = User.create!(email: "test#{i}@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+ puts user.inspect
+end
+user_count = User.count
+em "Created #{user_count} users"
+
 em "Creating random things"
 
 5.times do |i|
